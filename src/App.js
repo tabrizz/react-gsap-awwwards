1899fa4eca6f2eb4664d7ad1fe0f9263859f6b7f
tabrizz <kjetlag@gmail.com>
2020-05-17 00:34:13 -0500

    first commit tutorial by wrong akram

--- src/App.js
+++ src/App.js
@@ -1,12 +1,93 @@
-import React from "react";
-import "./styles/App.scss";
+import React, { useEffect, useState } from 'react'
+import { Route } from 'react-router-dom'
+import gsap from 'gsap'
 
-function App() {
-  return (
-    <div className='App'>
-      <h2>Hello this is your starter files </h2>
-    </div>
-  );
+import CaseStudies from './pages/caseStudies'
+import Approach from './pages/approach'
+import Services from './pages/services'
+import About from './pages/about'
+import Header from './components/header'
+import Home from './pages/home'
+import Navigation from './components/navigation'
+
+import './styles/App.scss'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    Component: Home,
+  },
+  {
+    path: '/case-studies',
+    name: 'Case Studies',
+    Component: CaseStudies,
+  },
+  {
+    path: '/approach',
+    name: 'Approach',
+    Component: Approach,
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    Component: Services,
+  },
+  {
+    path: '/abou-us',
+    name: 'About us',
+    Component: About,
+  },
+]
+
+function debounce(fn, ms) {
+  let timer
+  return () => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      timer = null
+      fn.apply(this, arguments)
+    }, ms)
+  }
 }
 
-export default App;
+function App() {
+  gsap.to('body', 0, { css: { visibility: 'visible' } })
+
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
+  useEffect(() => {
+    let vh = dimensions.height * 0.01
+    document.documentElement.style.setProperty('vh', `${vh}px`)
+
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      })
+    }, 1000)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
+  }, [dimensions])
+  return (
+    <>
+      <Header dimensions={dimensions} />
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path} component={Component}>
+            <Component />
+          </Route>
+        ))}
+      </div>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import gsap from 'gsap'
+
+import IntroOverlay from '../components/introOverlay'
+import Banner from '../components/banner'
+import Cases from '../components/cases'
+
+const tl = gsap.timeline()
+const homeAnimation = (completeAnimation) => {
+  return tl
+    .from('.line span', 1.8, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+    .to('.overlay-top', 1.6, {
+      height: 0,
+      ease: 'expo.inOut',
+      stagger: {
+        amount: 0.4,
+      },
+    })
+    .to('.overlay-bottom', 1.6, {
+      width: 0,
+      ease: 'expo.inOut',
+      delay: -0.8,
+      stagger: {
+        amount: 0.4,
+      },
+    })
+    .to('.intro-overlay', 0, { css: { display: 'none' } })
+    .from('.case-image img', 1.6, {
+      scale: 1.4,
+      ease: 'expo,inOut',
+      delay: -2,
+      stagger: {
+        amount: 0.4,
+      },
+      onComplete: completeAnimation,
+    })
+}
+
+const Home = () => {
+  const [animationComplete, setAnimationComplete] = useState(false)
+
+  const completeAnimation = () => {
+    setAnimationComplete(true)
+  }
+
+  useEffect(() => {
+    homeAnimation(completeAnimation)
+  }, [])
+
+  return (
+    <>
+      {!animationComplete ? <IntroOverlay /> : ''}
+      <Banner />
+      <Cases />
+    </>
+  )
+}
+
+export default Home
